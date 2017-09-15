10ec685a3279f6717ea826946db3502d19fe56b6
Your Name <nozomotoitech@gmail.com>
2017-09-15 19:33:17 +0900

    fix backup

--- src/main/Backup.js
+++ src/main/Backup.js
@@ -1,18 +1,10 @@
-var fs = require('fs');
-
-var Backup = function(args) {
-    var config = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
-    return {
-        getDatas: function(){
-            fs.readdir(config.directry.data, function (err, list) {
-                if (err) {
-                    console.error(err);
-                    return err;
-                }
-                else {
-                    return list
-                }
-            });
-        }
+const fs = require('fs');
+const path = require("path");
+const userHome = process.env[process.platform == "win32" ? "USERPROFILE" : "HOME"];
+const config = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
+const dirname = path.join(userHome, config.directry.data);
+module.exports = {
+    getDatas: function(){
+        return fs.readdirSync(dirname)
     }
-}
+};
